5c488be79e5da3bfdb509180ed6a4fb92f77f883
muthukumar-boomi <muthukumarboomi1@gmail.com>
2026-07-23 02:30:31 +0530

    Update payment_con.java

--- Pure-Goat-Farms/src/main/java/springboot/Pure/Goat/Farms/payment_con.java
+++ Pure-Goat-Farms/src/main/java/springboot/Pure/Goat/Farms/payment_con.java
@@ -8,6 +8,17 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.FontFactory;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.Rectangle;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
 
 import java.io.IOException;
 import java.time.LocalDate;
